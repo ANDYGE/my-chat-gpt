9696e20e7b835e18bcf3b147ab18a3c686c1c5fb
root <root@vultr.guest>
2023-05-21 08:37:03 +0000

    fix some bug

--- src/components/thinking.tsx
+++ src/components/thinking.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Thinking = () => {
     return (
         <div className="flex items-center space-x-2 mt-4">
-            <Image alt="thinking" src='/gpt.png' className="w-10 rounded-full" />
+            <Image width={40} height={40} alt="thinking" src='/gpt.png' className="w-10 rounded-full" />
             <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse" />
             <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse animation-delay-500" />
             <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse animation-delay-1000" />
